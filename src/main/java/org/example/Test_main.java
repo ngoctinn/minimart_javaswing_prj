adafaa20fef313e570bea81e6fb8dd870bdd1f3c
ngoctinn <Tinvg2918@gmail.com>
2025-02-19 08:48:18 +0700

    C

---
+++ src/main/java/org/example/Test_main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.GUI.LoginGUI;
+
+public class Test_main {
+    public static void main(String[] args) {
+        LoginGUI loginGUI = new LoginGUI();
+    }
+}
