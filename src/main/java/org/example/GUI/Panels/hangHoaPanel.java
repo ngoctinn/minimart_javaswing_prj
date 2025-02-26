913ec6dda07f8f1b89f8674ad9e5f078e72aa697
ngoctinn <Tinvg2918@gmail.com>
2025-02-26 09:11:55 +0700

    add new menu items and hangHoaPanel for enhanced UI functionality

---
+++ src/main/java/org/example/GUI/Panels/hangHoaPanel.java
@@ -0,0 +1,16 @@
+package org.example.GUI.Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class hangHoaPanel extends JPanel {
+    public hangHoaPanel() {
+        initGUI();
+    }
+
+    public void initGUI() {
+        this.setLayout(null);
+        setBackground(Color.BLUE);
+        this.setVisible(true);
+    }
+}
